21649a47c06e08e1bf7c7937e24ef2333cfe7b95
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2020-10-29 19:15:22 -0700

    test4 passed

--- Simple User-Level Thread Scheduler/test4.c
+++ Simple User-Level Thread Scheduler/test4.c
@@ -5,11 +5,13 @@
 void hello1() {
     int i;
     char sbuf[128];
+    sut_open("0.0.0.0", 3001);
     for (i = 0; i < 100; i++) {
 	sprintf(sbuf, "Hello world!, message from SUT-One i = %d \n", i);
 	sut_write(sbuf, strlen(sbuf));
 	sut_yield();
     }
+    sut_close();
     sut_exit();
 }
 
